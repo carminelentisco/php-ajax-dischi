504a3c522085b64b8e3889e657d2d6c8d7dfa453
carminelentisco <carminelentisco@gmail.com>
2020-05-20 16:55:30 +0200

    Correzione Lista

--- index.php
+++ index.php
@@ -6,28 +6,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/css/main.css">
 </head>
 <body>
 
-    <main>
-        <?php foreach ( $albums as $album ) {?>
+    <header class="header__page">
+        <div class="container">
+            <img src="dist/assets/spotify-logo.png" alt="Spotify Logo">
+        </div>
+    </header>
 
-        <div class="album">
-            
-            <div class="album__image">
-                <img class="poster" src=<?php echo $album['album_poster']?> alt="">   
+    <main>
+
+        <div class="album__container container">
+            <?php foreach ( $albums as $album ) {?>
+
+            <div class="album">
+                
+                <div class="album__image">
+                    <img class="poster" src=<?php echo $album['album_poster']?> alt="">   
+                </div>
+                
+                <div class="album__info">
+                    <h3><?php echo $album['album_title']; ?></h3>
+                    <span class="album__author"><?php echo $album['album_author']; ?></span>
+                    <span class="album__year"><?php echo $album['albumm_year']; ?></span>
+                </div>
+                
             </div>
-            
-            <div class="album__info">
-                <h3><?php echo $album['album_title']; ?></h3>
-                <span><?php echo $album['album_author']; ?></span>
-                <span><?php echo $album['albumm_year']; ?></span>
-            </div>
-            
+
+            <?php } ?>
         </div>
 
-        <?php } ?>
     </main>
 </body>
 </html>

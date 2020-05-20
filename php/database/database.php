76e60956d33d5b36015fddff62cd28afb15f36b4
carminelentisco <carminelentisco@gmail.com>
2020-05-20 15:44:27 +0200

    database - aggiunte le info sull immagine

---
+++ php/database/database.php
@@ -0,0 +1,66 @@
+<?php 
+
+    $albums = [
+        [
+            'album_poster' => "php/assets-database/aenima.png",
+            'album_title' => 'Ænima',
+            'album_author' => 'Ænima',
+            'albumm_year' => ' 1996',
+        ],
+        [
+            'album_poster' => 'php/assets-database/avril-lavigne.jpg',
+            'album_title' => 'Avril Lavigne',
+            'album_author' => 'Avril Lavigne',
+            'albumm_year' => '2010',
+        ],
+        [
+            'album_poster' => 'php/assets-database/daft-Punck-tron.jpg',
+            'album_title' => 'TRON Legacy',
+            'album_author' => 'Daft Punk',
+            'albumm_year' => '2010',
+        ],
+        [
+            'album_poster' => 'php/assets-database/gun-s-roses.jpeg',
+            'album_title' => 'Appetite For Destruction',
+            'album_author' => "Guns N' Roses",
+            'albumm_year' => '1987',
+        ],
+        [
+            'album_poster' => 'php/assets-database/qeen-II.png',
+            'album_title' => 'Queen II',
+            'album_author' => 'Queen',
+            'albumm_year' => '1974',
+        ],
+        [
+            'album_poster' => 'php/assets-database/michael-jackson-dangerous.jpg',
+            'album_title' => 'Dangerous',
+            'album_author' => 'Michael Jackson',
+            'albumm_year' => '1991',
+        ],
+        [
+            'album_poster' => 'php/assets-database/three-cheers-for-sweet-revenge.png',
+            'album_title' => 'Three Cheers for Sweet Revenge',
+            'album_author' => 'My Chemical Romance',
+            'albumm_year' => '2004',
+        ],
+        [
+            'album_poster' => 'php/assets-database/the-slow-rush.png',
+            'album_title' => 'TTame Impala',
+            'albumm_year' => '2020',
+        ],
+        [
+            'album_poster' => 'php/assets-database/norman-fucking-rockwell.png',
+            'album_title' => 'Norman Fucking Rockwell!',
+            'album_author' => 'Lana Del Rey',
+            'albumm_year' => '2019',
+        ],
+        [
+            'album_poster' => 'php/assets-database/fear-inoculum.png',
+            'album_title' => 'Fear Inoculum',
+            'album_author' => 'Tool',
+            'albumm_year' => '2019',
+        ],
+    ];
+
+
+?>

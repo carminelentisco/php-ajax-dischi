ee8202e0bf8d6667c5c8bb8fc10b4588bb18a3ec
carminelentisco <carminelentisco@gmail.com>
2020-06-05 09:48:04 +0200

    JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,163 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="/",r(r.s=0)}([function(e,t,r){r(1),e.exports=r(2)},function(e,t){jQuery(document).ready((function(e){var t=e(".album__container"),r=e("#album__template").html(),n=Handlebars.compile(r);e.ajax({url:"http://localhost/php-ajax-dischi/php/script/json_encode.php",mathod:"GET",success:function(e){for(key in e){var r=e[key],o={urlImage:r.album_poster,title:r.album_title,author:r.album_author,year:r.album_year},u=n(o);t.append(u)}},error:function(){console.error("Non è stato possibile accedere ai dati del database")}})}))},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+jQuery(document).ready(function ($) {
+  // Reference
+  var albumContainer = $('.album__container');
+  var inputText = $('.header__input-text'); // Handlebars init 
+
+  var source = $('#album__template').html();
+  var template = Handlebars.compile(source);
+  getAlbum(template, albumContainer);
+  inputText.keyup(function (e) {
+    var text = inputText.val();
+    getAlbum(text, template, albumContainer);
+  });
+});
+
+function getAlbum(text, template, albumContainer) {
+  $.ajax({
+    url: 'http://localhost/php-ajax-dischi/php/script/json_encode.php',
+    mathod: 'GET',
+    success: function success(res) {
+      if (text !== '') {
+        console.log('si');
+      }
+
+      for (key in res) {
+        var item = res[key];
+        var data = {
+          urlImage: item.album_poster,
+          title: item.album_title,
+          author: item.album_author,
+          year: item.album_year
+        };
+        var outputHtml = template(data);
+        albumContainer.append(outputHtml);
+      }
+    },
+    error: function error() {
+      console.error('Non è stato possibile accedere ai dati del database');
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "./src/scss/main.scss":
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!***************************************************!*\
+  !*** multi ./src/js/main.js ./src/scss/main.scss ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! C:\MAMP\htdocs\php-ajax-dischi\src\js\main.js */"./src/js/main.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\php-ajax-dischi\src\scss\main.scss */"./src/scss/main.scss");
+
+
+/***/ })
+
+/******/ });
--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,35 @@ jQuery(document).ready( ($) => {
 
     // Reference
     var albumContainer = $('.album__container');
+    var inputText = $('.header__input-text');
 
     // Handlebars init 
     var source = $('#album__template').html();
     var template = Handlebars.compile(source);
 
+    getAlbum (template, albumContainer);
+
+    inputText.keyup(function (e) {
+        var text = inputText.val();
+        getAlbum (text, template, albumContainer);
+    });
+
+});
+
+function getAlbum (text, template, albumContainer) {
+    
     $.ajax({
         url: 'http://localhost/php-ajax-dischi/php/script/json_encode.php',
         mathod: 'GET',
-        success: function (res) {
+        success: (res) => {
             
+            if( text !== '') {
+                console.log('si');
+            }
+
             for ( key in res ) {
                 var  item = res[key];
+
                 var data = {
                     urlImage: item.album_poster,
                     title: item.album_title,
@@ -29,4 +46,4 @@ jQuery(document).ready( ($) => {
             console.error('Non è stato possibile accedere ai dati del database')
         }
     });
-});
+}

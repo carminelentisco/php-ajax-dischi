9a6663d93de4ee641b5ed504c9a7faf3b63ccd4a
carminelentisco <carminelentisco@gmail.com>
2020-06-05 09:48:19 +0200

    index

---
+++ php/index.php
@@ -0,0 +1,43 @@
+<?php include_once __DIR__ . '/php/database/database.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/main.css">
+</head>
+<body>
+
+    <header class="header__page">
+        <div class="container">
+            <img src="dist/assets/spotify-logo.png" alt="Spotify Logo">
+        </div>
+    </header>
+
+    <main>
+
+        <div class="album__container container">
+            <?php foreach ( $albums as $album ) {?>
+            <!-- ALBUM -->
+            <div class="album">
+                <!-- ALBUM IMAGE -->
+                <div class="album__image">
+                    <img class="poster" src=<?php echo $album['album_poster']?> alt="">   
+                </div>
+                <!-- ALBUM INFO -->
+                <div class="album__info">
+                    <h3><?php echo $album['album_title']; ?></h3>
+                    <small class="album__author"><?php echo $album['album_author']; ?></small>
+                    <span class="album__year"><?php echo $album['albumm_year']; ?></span>
+                </div>      
+            </div>
+            <?php } ?>
+        </div>
+
+    </main>
+
+</body>
+</html>
